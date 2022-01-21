4de01d1e84e69688092f2f7f7f278011553799ba
Jay Philip <jayphilipcamillo@gmail.com>
2022-01-21 12:21:24 +0800

    Go to root url when logo is clicked

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,19 @@
-import { Box, Container, HStack, Link, Spacer } from "@chakra-ui/react";
+import { Box, Container, HStack, Spacer } from "@chakra-ui/react";
 import Image from "next/image";
 import MobileNav from "./MobileNav";
 import Nav from "./Nav";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
       <Box as='footer' position='fixed' w='full' bgColor='#212121' zIndex={2}>
         <Container maxW='container.lg' display='flex' alignItems='center' h='100'>
-          <Box color='white'>
-            <Image src='/jp.svg' alt='logo' height='50' width='64' />
-          </Box>
+          <Link href='/' passHref>
+            <Box cursor='pointer'>
+              <Image src='/jp.svg' alt='logo' height='50' width='64' />
+            </Box>
+          </Link>
           <Spacer />
           <Box display={{ base: "none", md: "block" }}>
             <Nav />
